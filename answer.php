16e1d6dd14d4f634c45a63b780d1dd7d28bb5e30
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-20 01:13:02 +0000

    added php

--- answer.php
+++ answer.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!--ICD2O Unit 2-02 HTML-->
+<!--ICD2O Unit 1-08 HTML-->
 <html lang="en-ca">
   <head>
     <meta charset="utf-8" />
-    <meta name="description" content="The math program, PHP" />
+    <meta name="description" content="The program, PHP" />
     <meta name="keywords" content="ICD2O, mths" />
     <meta name="author" content="Aimar Fernandez" />
     <meta name="viewport" content="width+device-width, initial-scale=.1.0" />
@@ -20,7 +20,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
     <link rel="manifest" href="site.webmanifest" />
-    <title>The Math Program, PHP</title>
+    <title>The Program, PHP</title>
   </head>
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
@@ -35,31 +35,11 @@
         <div>
           <img src="./images/notebook-animation.svg" alt="Laptop Image">
         </div>
+        <div class="page-content">The answer is ...</div>
         <br />
-        <div class="page-content">Instructions Here</div>
-        <br />
-        <!-- Numeric Textfield with Floating Label -->
-        <form action="#">
-          <div
-            class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"
-          >
-            <input
-              class="mdl-textfield__input"
-              type="text"
-              pattern="-?[0-9]*(\.[0-9]+)?"
-              id="sample4"
-            />
-            <label class="mdl-textfield__label" for="sample4"> Number...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-        </form>
-        <!-- Raised button with ripple -->
-        <button
-          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-          type="submit"
-        >
-          Calculate
-        </button>
+        <div class="page-content-answer">
+          <a href="./index.php"> Return ...<a>
+        </div>
         <br />
       </main>
     </div>
